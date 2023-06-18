47f3e792662bcbd4a98459df90f66c54f1023c21
homy <homycoder@gmail.com>
2023-06-18 21:35:18 +0300

    fwefwfe

--- middlewares/middleware.py
+++ middlewares/middleware.py
@@ -9,23 +9,20 @@ from utils.db_api import Users
 class AuthenticationMiddleware(BaseMiddleware):
     async def on_process_message(self, message: Message, data: dict):
 
-        #  if message.chat.type != "private":
-          #   raise CancelHandler()
+        if message.chat.type == "private":
+            user_id = message.chat.id
+            logger.debug(f'New message from @{message.from_user.username}:{user_id}')
 
-        user_id = message.chat.id
-        logger.debug(f'New message from @{message.from_user.username}:{user_id}')
+            user_info = Users.get_or_none(Users.user_id == user_id)
 
-        user_info = Users.get_or_none(Users.user_id == user_id)
+            if not user_info:
+                if not message.text == "/start":
+                        return await message.answer("Введите /start")
+                else:
+                    return logger.error(f"user @{message.from_user.username} is not authorized..")
 
-        if not user_info:
-            if not message.text == "/start":
-                if message.chat.type == "private":
-                    return await message.answer("Введите /start")
-            else:
-                return logger.error(f"user @{message.from_user.username} is not authorized..")
-
-        if user_info.banned:
-            raise CancelHandler()
+            if user_info.banned:
+                raise CancelHandler()
 
 
 
